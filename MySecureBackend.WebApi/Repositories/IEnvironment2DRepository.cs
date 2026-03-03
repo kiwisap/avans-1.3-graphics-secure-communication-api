dfeb9a56855ff89af425d539ef50b6f8b85da5d1
FRATZIE\jesse <jesse.jutten@gmail.com>
2026-03-03 12:29:34 +0100

    Removed examples

--- MySecureBackend.WebApi/Repositories/IEnvironment2DRepository.cs
+++ MySecureBackend.WebApi/Repositories/IEnvironment2DRepository.cs
@@ -5,8 +5,12 @@ namespace MySecureBackend.WebApi.Repositories;
 public interface IEnvironment2DRepository
 {
     Task<IEnumerable<Environment2D>> GetAllByUserIdAsync(string userId);
+
     Task<Environment2D?> GetByIdAsync(int id, string userId);
+
     Task<Environment2D> CreateAsync(Environment2D environment);
+
     Task<bool> UpdateAsync(Environment2D environment, string userId);
+
     Task<bool> DeleteAsync(int id, string userId);
 }
--- MySecureBackend.WebApi/Repositories/IObject2DRepository.cs
+++ MySecureBackend.WebApi/Repositories/IObject2DRepository.cs
@@ -5,8 +5,12 @@ namespace MySecureBackend.WebApi.Repositories;
 public interface IObject2DRepository
 {
     Task<IEnumerable<Object2D>> GetAllByEnvironmentIdAsync(int environmentId, string userId);
+
     Task<Object2D?> GetByIdAsync(int id, string userId);
+
     Task<Object2D> CreateAsync(Object2D obj, string userId);
+
     Task<bool> UpdateAsync(Object2D obj, string userId);
+
     Task<bool> DeleteAsync(int id, string userId);
 }
